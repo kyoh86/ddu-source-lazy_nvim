9cb6b41760d70b47b73f29c05016c291983bd0ef
GitHub <noreply@github.com>
2023-08-26 04:00:17 +0000

    chore(deps): Update deno dependencies

--- denops/@ddu-sources/lazy_nvim.ts
+++ denops/@ddu-sources/lazy_nvim.ts
@@ -1,16 +1,16 @@
-import type { GatherArguments } from "https://deno.land/x/ddu_vim@v3.5.1/base/source.ts";
+import type { GatherArguments } from "https://deno.land/x/ddu_vim@v3.6.0/base/source.ts";
 import type { ActionData as FileActionData } from "https://deno.land/x/ddu_kind_file@v0.5.3/file.ts";
 import type {
   Actions,
   DduItem,
   Item,
-} from "https://deno.land/x/ddu_vim@v3.5.1/types.ts";
+} from "https://deno.land/x/ddu_vim@v3.6.0/types.ts";
 
 import {
   ActionFlags,
   BaseSource,
-} from "https://deno.land/x/ddu_vim@v3.5.1/types.ts";
-import { Denops, fn } from "https://deno.land/x/ddu_vim@v3.5.1/deps.ts";
+} from "https://deno.land/x/ddu_vim@v3.6.0/types.ts";
+import { Denops, fn } from "https://deno.land/x/ddu_vim@v3.6.0/deps.ts";
 import { join } from "https://deno.land/std@0.200.0/path/mod.ts";
 import { echoerr, pipe } from "../ddu-source-lazy_nvim/message.ts";
 

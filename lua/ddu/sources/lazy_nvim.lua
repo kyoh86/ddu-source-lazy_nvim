a63ce05b67168a1362046bd805a62b20a116ab45
uga-rosa <uga6603@gmail.com>
2023-12-23 15:17:27 +0900

    Use LazyPlugin.dir as path of the plugin

--- lua/ddu/sources/lazy_nvim.lua
+++ lua/ddu/sources/lazy_nvim.lua
@@ -1,14 +1,13 @@
 local M = {}
 
 function M.plugins_action_data()
-  local lazy_root = require("lazy.core.config").options.root
   local plugins = {}
   for _, plugin in pairs(require("lazy.core.config").plugins) do
     table.insert(plugins, { -- it forms ddu Item<FileActionData & {spec: string; lazy: boolean}>
       word = plugin[1],
       action = {
         name = plugin.name,
-        path = vim.fs.joinpath(lazy_root, plugin.name),
+        path = plugin.dir,
         isDirectory = true,
         url = plugin.url,
         spec = plugin[1],

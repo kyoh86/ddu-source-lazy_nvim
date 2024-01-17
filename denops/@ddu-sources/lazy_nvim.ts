3020a4097d850c41fa649eeb7682895888c29753
kyoh86 <me@kyoh86.dev>
2024-01-18 00:04:43 +0900

    update dependencies

--- denops/@ddu-sources/lazy_nvim.ts
+++ denops/@ddu-sources/lazy_nvim.ts
@@ -12,8 +12,8 @@ import {
 } from "https://deno.land/x/ddu_vim@v3.9.0/types.ts";
 import { Denops, fn } from "https://deno.land/x/ddu_vim@v3.9.0/deps.ts";
 import { join } from "https://deno.land/std@0.212.0/path/mod.ts";
-import { echomsg } from "https://denopkg.com/kyoh86/denops-util@v0.0.5/echomsg.ts";
-import { echoallCommand } from "https://denopkg.com/kyoh86/denops-util@v0.0.5/command.ts";
+import { echomsg } from "https://denopkg.com/kyoh86/denops-util@v0.0.6/echomsg.ts";
+import { echoallCommand } from "https://denopkg.com/kyoh86/denops-util@v0.0.6/command.ts";
 
 type ActionData = FileActionData & LazyPlugin;
 

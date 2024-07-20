d82cb7e431d7e81d1958772831d2196f3c8f1b7b
GitHub <noreply@github.com>
2024-07-20 09:02:28 +0000

    chore(deps): Update deno dependencies

--- denops/ddu-kind-lazy_nvim_plugin/main.ts
+++ denops/ddu-kind-lazy_nvim_plugin/main.ts
@@ -1,5 +1,5 @@
-import { Denops } from "https://deno.land/x/denops_std@v6.5.0/mod.ts";
-import * as fn from "https://deno.land/x/denops_std@v6.5.0/function/mod.ts";
+import { Denops } from "https://deno.land/x/denops_std@v6.5.1/mod.ts";
+import * as fn from "https://deno.land/x/denops_std@v6.5.1/function/mod.ts";
 import {
   ensure,
   is,

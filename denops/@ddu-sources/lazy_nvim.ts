0e253e51c92e768998e1906cc6f1ccbcdc9bb7fd
kyoh86 <me@kyoh86.dev>
2025-09-14 02:56:29 +0900

    Update dependencies

--- denops/@ddu-sources/lazy_nvim.ts
+++ denops/@ddu-sources/lazy_nvim.ts
@@ -1,15 +1,15 @@
-import type { GatherArguments } from "jsr:@shougo/ddu-vim@~10.3.0/source";
-import type { ActionData as FileActionData } from "jsr:@shougo/ddu-kind-file@~0.9.0";
+import type { GatherArguments } from "jsr:@shougo/ddu-vim@~11.1.0/source";
+import type { ActionData as FileActionData } from "jsr:@shougo/ddu-kind-file@~1.0.0";
 import {
   ActionFlags,
   type Actions,
   type DduItem,
   type Item,
-} from "jsr:@shougo/ddu-vim@~10.3.0/types";
-import { BaseSource } from "jsr:@shougo/ddu-vim@~10.3.0/source";
+} from "jsr:@shougo/ddu-vim@~11.1.0/types";
+import { BaseSource } from "jsr:@shougo/ddu-vim@~11.1.0/source";
 
-import type { Denops } from "jsr:@denops/std@~7.6.0";
-import * as fn from "jsr:@denops/std@~7.6.0/function";
+import type { Denops } from "jsr:@denops/std@~8.0.0";
+import * as fn from "jsr:@denops/std@~8.0.0/function";
 import { echomsg } from "jsr:@kyoh86/denops-util@~0.1.0/echomsg";
 import { ensure, is, maybe } from "jsr:@core/unknownutil@~4.3.0";
 
